58ea3476e1742ccb86d50bbfcffca3f6b5ed66e6
LiMiSu <1102523711@qq.com>
2020-06-05 11:23:55 +0800

    '自己写'

--- src/dom.js
+++ src/dom.js
@@ -1,140 +1,165 @@
 window.dom = {
+    //创建插入标签
     create(string) {
         // const div = dom.create('     <div><span>1</span></div>');
         const container = document.createElement('template');//创建一个标签容器里面放代码，如果是div，那么里面不能放td等，所以最好用template标签，能放任何元素
         container.innerHTML = string.trim();//trim()函数是去掉字符串两边的空格，innerHTML当有空格时会变成文本节点，firstChild拿到的就是文本节点
         return container.content.firstChild;//template里面的元素只能通过.content.xxxChild拿到，不能children
     },
+    //在node后面插入node2  原生的after是新功能，还不兼容，用insertBefore:在之前插入
     after(node, node2) {
-        node.parentNode.insertBefore(node2, node.nextSibling);
+        // console.log(node.nextSibling);
+        node.parentNode.insertBefore(node2, node.nextSibling);//插到我的后面就是插到我下一个兄弟的前面，// 当最后一个没有兄弟时也可
     },
+    //在前面插入
     before(node, node2) {
         node.parentNode.insertBefore(node2, node);
     },
+    //新增儿子
     append(parent, node) {
-        parent.appendChild(node)
+        parent.appendChild(node);
     },
-    wrap(node, parent) {//加一层父级
-        dom.before(node, parent)
-        dom.append(parent, node)
+    //新增爸爸：先把要加的插到我前面/后面也行，再把我插到它里面
+    wrap(node, parent) {
+        dom.before(node, parent);
+        dom.append(parent, node);
     },
     remove(node) {
-        node.parentNode.removeChild(node)
-        return node
+        // node.remove() //这个接口比较新 IE可能不支持，所以换一个让爸爸删
+        node.parentNode.removeChild(node);
+        return node;//返回，保留它可以用
     },
     empty(node) {
-        const array = []
-        let x = node.firstChild
-        while (x) {
-            array.push(dom.remove(node.firstChild))
-            x = node.firstChild
+        // node.innerHTML=''; //这样也是可以的，但是拿不到被清空掉的元素的引用，有时候需要拿到
+        // const childNodes=node.childNodes
+        //上面可以用ES6语法
+        const {childNodes} = node;
+        const array = [];
+        // for (let i = 0; i < childNodes.length; i++) {
+        //     // console.log(childNodes.length);//边删除length会实时改变
+        //     console.log(childNodes[i]);
+        //     array.push(dom.remove(childNodes[i]));
+        // }
+        let first = node.firstChild;
+        while (first) {
+            array.push(dom.remove(node.firstChild));
+            first = node.firstChild;//第一个删了第二个跑上去变第一个
         }
-        return array
+        return array;
     },
-    attr(node, name, value) { // 重载
-        if (arguments.length === 3) {
-            node.setAttribute(name, value)
+    //改
+    attr(node, name, value) {
+        if (arguments.length === 3) {//重载：根据不同的参数个数做不同的事情
+            node.setAttribute(name, value);
         } else if (arguments.length === 2) {
-            return node.getAttribute(name)
+            return node.getAttribute(name);
         }
     },
-    text(node, string) { // 适配
+    text(node, string) {
         if (arguments.length === 2) {
-            if ('innerText' in node) {
-                node.innerText = string
+            // if (node.innerText){
+            if ('innerText' in node) {//适配，兼容不同的浏览器
+                node.innerText = string;//Firefox/chrome
             } else {
-                node.textContent = string
+                node.textContent = string;//IE
             }
         } else if (arguments.length === 1) {
             if ('innerText' in node) {
-                return node.innerText
+                return node.innerText;
             } else {
-                return node.textContent
+                return node.textContent;
             }
         }
     },
     html(node, string) {
         if (arguments.length === 2) {
-            node.innerHTML = string
-        } else if (arguments.length === 1) {
-            return node.innerHTML
+            node.innerHTML = string;
+        } else if (arguments.length == 1) {
+            return node.innerHTML;
         }
     },
     style(node, name, value) {
         if (arguments.length === 3) {
-            // dom.style(div, 'color', 'red')
-            node.style[name] = value
+            // dom.style(test,'border','1px solid red');
+            node.style[name] = value;
         } else if (arguments.length === 2) {
-            if (typeof name === 'string') {
-                // dom.style(div, 'color')
-                return node.style[name]
+            if (typeof name === "string") {
+                //dom.style(test.'border');
+                return node.style[name];
+                // }else if (typeof name==="object"){
             } else if (name instanceof Object) {
-                // dom.style(div, {color: 'red'})
-                const object = name
-                for (let key in object) {
-                    node.style[key] = object[key]
+                //dom.style(test, {border: '1px solid red',color:'blue'});
+                for (let key in name) {
+                    //key:border / color
+                    //node.style.border =...
+                    //node.style.color =...
+                    node.style[key] = name[key];//key是一个变量所以用[]
                 }
             }
         }
+
     },
-    class: {
+    class: {//classList用法
         add(node, className) {
-            node.classList.add(className)
+            node.classList.add(className);
         },
         remove(node, className) {
-            node.classList.remove(className)
+            node.classList.remove(className);
         },
         has(node, className) {
-            return node.classList.contains(className)
+            return node.classList.contains(className);
         }
     },
     on(node, eventName, fn) {
-        node.addEventListener(eventName, fn)
+        node.addEventListener(eventName, fn);
     },
     off(node, eventName, fn) {
-        node.removeEventListener(eventName, fn)
+        node.removeEventListener(eventName, fn);
     },
-    find(selector, scope) {
-        return (scope || document).querySelectorAll(selector)
+    //
+    find(selector, scope) {//querySelectorAll方法
+        return (scope || document).querySelectorAll(selector);//秒啊，不过那也只能是id啊，其他可以存个变量再套进来
     },
     parent(node) {
-        return node.parentNode
+        return node.parentNode;
     },
     children(node) {
-        return node.children
+        return node.children;
     },
     siblings(node) {
-        return Array.from(node.parentNode.children)
-            .filter(n => n !== node)
+        return Array.from(node.parentNode.childNodes).filter(n => n !== node)//过滤掉自己
     },
+    //找弟弟
     next(node) {
-        let x = node.nextSibling
-        while (x && x.nodeType === 3) {
-            x = x.nextSibling
+        let next = node.nextSibling;
+        if (next && next.nodeType === 3) {
+            next = next.nextSibling;
         }
-        return x
+        return next;
     },
-    previous(node) {
-        let x = node.previousSibling
-        while (x && x.nodeType === 3) {
-            x = x.previousSibling
+    previous(node){
+        let last = node.previousSibling;
+        if (last && last.nodeType === 3) {
+            last = last.previousSibling;
         }
-        return x
+        return last;
     },
-    each(nodeList, fn) {
-        for (let i = 0; i < nodeList.length; i++) {
-            fn.call(null, nodeList[i])
+    //遍历
+    each(nodeList,fn){
+        for (let i=0;i<nodeList.length;i++){
+            fn.call(null,nodeList[i]);
         }
     },
-    index(node) {
-        const list = dom.children(node.parentNode)
-        let i
-        for (i = 0; i < list.length; i++) {
-            if (list[i] === node) {
+    //获取排行
+    index(node){
+        const list=dom.children(node.parentNode);
+        let i;
+        for (i=0;i<list.length;i++){
+            if (list[i]===node){
                 break
             }
         }
-        return i
+        return i;
     }
 };
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,61 @@
-const div = dom.find('#test>.red')[0] // 获取对应的元素
-dom.style(div, 'color', 'red') // 设置 div.style.color
+//创建
+const div = dom.create("       <td>hi</td>");
+// console.log(div);
+//在之后插入
+dom.after(test, div);
+// console.log(test);
+//加父亲
+const div2 = dom.create('<div id="parent"></div>');
+dom.wrap(test, div2);
+//删除
+// const node=dom.remove(div2);
+// console.log(node);
+//清空
+// console.log(window.empty)
+const nodes = dom.empty(window.empty);
+// console.log(nodes);
 
-const divList = dom.find('.red') // 获取多个 div.red 元素
-console.log(div)
-console.log(divList)
-dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
+//改
+dom.attr(test, 'title', 'Hi, I am A');
+//读
+const title = dom.attr(test, 'title');
+// console.log(title);
+//设置读取文本内容
+dom.text(test, '你好');
+dom.text(test);
+//改样式
+dom.style(test, {border: '1px solid red', color: 'blue'});
+//读样式
+// console.log(dom.style(test,'border'));
+dom.style(test, 'border', '1px solid blue');
+//添加class
+dom.class.add(test, 'red');
+dom.class.add(test, 'blue');
+dom.class.remove(test, 'blue');
+// console.log(dom.class.has(test,'blue'));
+//事件
+// const fn=()=>{
+//     console.log('点击了');
+// }
+// dom.on(test,'click',fn);
+// dom.off(test,'click',fn);
+
+//找
+// console.log(dom.find('#test'));
+console.log(dom.find('#test')[0]);
+console.log(dom.find('.red', test4));
+const a = document.querySelector('.test3');
+console.log(dom.find('.red', a));
+// console.log(test4.querySelectorAll('.red'))
+// console.log(document.querySelector('.test3').querySelectorAll('.red'))
+
+
+//找爸爸
+console.log(dom.parent(test));
+console.log(dom.siblings(dom.find('#test4')[0]));
+
+console.log(dom.next(dom.find('#test5')[0]));
+console.log(dom.previous(dom.find('#test5')[0]));
+const t = dom.find('#travel')[0];
+dom.each(dom.children(t),(n)=>dom.style(n,'color','red'));
+console.log(dom.index(test4));
